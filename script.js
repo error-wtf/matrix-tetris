e559dbc4717659799a7d11207cf277b2b51bcc2e
Lino Casu <118898167+error-wtf@users.noreply.github.com>
2025-12-30 23:03:47 +0100

    Use proven working collide function

--- script.js
+++ script.js
@@ -469,29 +469,14 @@ function drawNext() {
 }
 
 function collide(arena, player) {
-    const m = player.matrix;
-    const o = player.pos;
-    
-    for (let y = 0; y < m.length; y++) {
-        for (let x = 0; x < m[y].length; x++) {
-            if (m[y][x] !== 0) {
-                const newY = y + o.y;
-                const newX = x + o.x;
-                
-                // Check bottom boundary
-                if (newY >= ROWS) {
-                    return true;
-                }
-                
-                // Check left/right boundaries
-                if (newX < 0 || newX >= COLS) {
-                    return true;
-                }
-                
-                // Check collision with placed blocks
-                if (newY >= 0 && arena[newY] && arena[newY][newX] !== 0) {
-                    return true;
-                }
+    const [m, o] = [player.matrix, player.pos];
+    for (let y = 0; y < m.length; ++y) {
+        for (let x = 0; x < m[y].length; ++x) {
+            if (m[y][x] !== 0 &&
+               (arena[y + o.y] === undefined ||
+                arena[y + o.y][x + o.x] === undefined ||
+                arena[y + o.y][x + o.x] !== 0)) {
+                return true;
             }
         }
     }
